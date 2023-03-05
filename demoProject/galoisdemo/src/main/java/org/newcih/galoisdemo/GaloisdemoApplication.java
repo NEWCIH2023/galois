b5d4adbad03d08909e17c11bc61ce6165c1cc8b9
newcih <newcih@foxmail.com>
2023-03-05 17:35:24 +0800

    autoSync

--- demoProject/galoisdemo/src/main/java/org/newcih/galoisdemo/GaloisdemoApplication.java
+++ demoProject/galoisdemo/src/main/java/org/newcih/galoisdemo/GaloisdemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-//@MapperScan(basePackages = "org.newcih.galoisdemo.dao")
+@SpringBootApplication()
+@MapperScan(basePackages = "org.newcih.galoisdemo.dao")
 public class GaloisdemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/newcih/utils/FileUtils.java
+++ src/main/java/org/newcih/utils/FileUtils.java
@@ -13,6 +13,5 @@ public class FileUtils {
             return false;
         }
 
-        return file.
     }
 }
