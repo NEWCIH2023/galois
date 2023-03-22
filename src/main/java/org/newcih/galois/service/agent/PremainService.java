60e0c0fe63ca06c8dd10294a47405477bdb3d388
liuguangsheng <1573390117@qq.com>
2023-03-22 18:46:27 +0800

    autoSync

--- src/main/java/org/newcih/galois/service/agent/PremainService.java
+++ src/main/java/org/newcih/galois/service/agent/PremainService.java
@@ -103,7 +103,7 @@ public class PremainService {
         .map(Object::toString)
         .collect(joining(","));
 
-    logger.info("Now enable Plugins [{}]，and start FileWatchListeners [{}]", enableAgentNames,
+    logger.info("Now enable Plugins [{}], and start FileWatchListeners [{}]", enableAgentNames,
         listenerNames);
   }
 
