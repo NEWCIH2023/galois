88d62815b13407e5b11e53d7c87baccf7b6b3348
newcih <newcih@foxmail.com>
2023-04-05 10:20:01 +0800

    autoSync

---
+++ src/main/java/io/liuguangsheng/galois/utils/GaloisLog.java
@@ -0,0 +1,364 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) [2023] [$user]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.liuguangsheng.galois.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+
+/**
+ * The type Galois log.
+ */
+public class GaloisLog implements Logger {
+    private Logger logger;
+
+    /**
+     * Instantiates a new Galois log.
+     *
+     * @param clazz the clazz
+     */
+    public GaloisLog(Class<?> clazz) {
+        logger = LoggerFactory.getLogger(clazz);
+    }
+
+    /**
+     * Instantiates a new Galois log.
+     *
+     * @param name the name
+     */
+    public GaloisLog(String name) {
+        logger = LoggerFactory.getLogger(name);
+    }
+
+    @Override
+    public String getName() {
+        return logger.getName();
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+        return logger.isTraceEnabled();
+    }
+
+    @Override
+    public void trace(String msg) {
+        logger.trace(msg);
+    }
+
+    @Override
+    public void trace(String format, Object arg) {
+        logger.trace(format, arg);
+    }
+
+    @Override
+    public void trace(String format, Object arg1, Object arg2) {
+        logger.trace(format, arg1, arg2);
+    }
+
+    @Override
+    public void trace(String format, Object... arguments) {
+        logger.trace(format, arguments);
+    }
+
+    @Override
+    public void trace(String msg, Throwable t) {
+        logger.trace(msg, t);
+    }
+
+    @Override
+    public boolean isTraceEnabled(Marker marker) {
+        return logger.isTraceEnabled(marker);
+    }
+
+    @Override
+    public void trace(Marker marker, String msg) {
+        logger.trace(marker, msg);
+
+    }
+
+    @Override
+    public void trace(Marker marker, String format, Object arg) {
+        logger.trace(marker, format, arg);
+
+    }
+
+    @Override
+    public void trace(Marker marker, String format, Object arg1, Object arg2) {
+        logger.trace(marker, format, arg1, arg2);
+
+    }
+
+    @Override
+    public void trace(Marker marker, String format, Object... argArray) {
+        logger.trace(marker, format, argArray);
+
+    }
+
+    @Override
+    public void trace(Marker marker, String msg, Throwable t) {
+        logger.trace(marker, msg, t);
+
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+        return logger.isDebugEnabled();
+    }
+
+    @Override
+    public void debug(String msg) {
+        logger.debug(msg);
+    }
+
+    @Override
+    public void debug(String format, Object arg) {
+
+    }
+
+    @Override
+    public void debug(String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void debug(String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void debug(String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isDebugEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void debug(Marker marker, String msg) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String format, Object arg) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+        return false;
+    }
+
+    @Override
+    public void info(String msg) {
+
+    }
+
+    @Override
+    public void info(String format, Object arg) {
+
+    }
+
+    @Override
+    public void info(String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void info(String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void info(String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isInfoEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void info(Marker marker, String msg) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String format, Object arg) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+        return false;
+    }
+
+    @Override
+    public void warn(String msg) {
+
+    }
+
+    @Override
+    public void warn(String format, Object arg) {
+
+    }
+
+    @Override
+    public void warn(String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void warn(String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void warn(String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isWarnEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void warn(Marker marker, String msg) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String format, Object arg) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+        return false;
+    }
+
+    @Override
+    public void error(String msg) {
+
+    }
+
+    @Override
+    public void error(String format, Object arg) {
+
+    }
+
+    @Override
+    public void error(String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void error(String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void error(String msg, Throwable t) {
+
+    }
+
+    @Override
+    public boolean isErrorEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void error(Marker marker, String msg) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String format, Object arg) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String format, Object arg1, Object arg2) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String format, Object... arguments) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String msg, Throwable t) {
+
+    }
+}
